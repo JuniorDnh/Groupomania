770fdfe0be6f96359118d463fb0369388b58bc4d
SonimJunior <sonim7@hotmail.fr>
2022-01-25 11:59:47 +0100

    commit

--- back-end/routes/like.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const likeCtrl = require("../controllers/like");
-
-router.post("/posts/:id/likes", likeCtrl.likePost);
-router.get("/likes/:userId", likeCtrl.getAllLikes);
-
-module.exports = router;
