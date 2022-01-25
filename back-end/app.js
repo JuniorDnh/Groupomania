663abb9d42c500fa60bc0bc536854b52f2427fc9
SonimJunior <sonim7@hotmail.fr>
2022-01-25 12:29:19 +0100

    commit

--- back-end/app.js
+++ back-end/app.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const helmet = require("helmet");
 const userRoutes = require("./routes/user");
 const postRoutes = require("./routes/post");
-const likeRoutes = require("./routes/like");
 const commentRoutes = require("./routes/comment");
 const db = require("./models");
 const app = express();
@@ -26,10 +25,17 @@ db.sequelize
     console.log("Impossible de se connecter à la base de données: ", error);
   });
 
+//middleware pour résoudre les erreurs de CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATH, OPTIONS');
+  next();
+});
+
 //ROUTES
 app.use("/api/user", userRoutes);
 app.use("/api/posts", postRoutes);
-app.use("/api", likeRoutes);
 app.use("/api", commentRoutes); 
 
 module.exports = app;
